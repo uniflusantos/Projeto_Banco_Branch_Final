0625d55ad96bfdf9a2fd5fd5f59aaa72f7da612f
uniftsantos <146483236+uniftsantos@users.noreply.github.com>
2023-10-17 16:08:22 -0300

    funcao de buscar cpf, onde se o cpf existir ele me retorna a posição, se não retorna -1

--- biblioteca.c
+++ biblioteca.c
@@ -11,3 +11,17 @@ void limpa_buffer(){
     int c;
     while ((c = getchar()) != '\n' && c != EOF) { }
 }
+
+int buscar_cpf(long cpf, struct contas*t, int cont){
+    int saida = -1;
+    for(int i = 0; i < cont; i++){
+        if(t[i].cpf == cpf){
+            saida = i;
+        }
+        //printf("indice :%d \n", i); (debug)
+        //printf("cpf1: %ld \n", t[i].cpf); (debug)
+        //printf("cpf2: %ld \n", cpf);(debug)
+        //printf("saida %d \n", saida); (debug)
+    }
+    return saida;
+}
