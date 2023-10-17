52a17b6bc170a3b4e24352121d994816d0788fa2
uniflusantos <146308610+uniflusantos@users.noreply.github.com>
2023-10-17 15:55:12 -0300

    Struct das contas

--- biblioteca.h
+++ biblioteca.h
@@ -5,4 +5,12 @@
 #ifndef UNTITLED19_BIBLIOTECA_H
 #define UNTITLED19_BIBLIOTECA_H
 
+struct contas{
+    char nome[100];
+    long cpf;
+    int tipo_conta;
+    int valor_inicial;
+    char senha[100];
+};
+
 #endif //UNTITLED19_BIBLIOTECA_H
